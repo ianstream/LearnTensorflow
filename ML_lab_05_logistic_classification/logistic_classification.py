647b5fa7acf4ca21b5c84698c006fec1275efbcf
ianstream <ianstream@naver.com>
2016-06-13 23:55:45 +0900

    5장 결과 확인코드 추가

--- ML_lab_05_logistic_classification/logistic_classification.py
+++ ML_lab_05_logistic_classification/logistic_classification.py
@@ -3,12 +3,14 @@
 import tensorflow as tf
 import numpy as np
 
-xy = np.loadtxt('train.txt', unpack=True, dtype='float32')
+# # 시군구,전용면적(㎡),거래금액(만원),건축년도
+# xy = np.genfromtxt('test.csv', delimiter=',', unpack=True, dtype='float32')
+xy = np.loadtxt('test.txt', unpack=True, dtype='float32')
 x_data = xy[0:-1]
-y_data = xy[-1];
+y_data = xy[-1]
 
-X = tf.placeholder(tf.floate32)
-Y = tf.placeholder(tf.floate32)
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
 
 W = tf.Variable(tf.random_uniform([1, len(x_data)], -1.0, 1.0))
 
@@ -35,6 +37,12 @@ sess.run(init)
 for step in range(2001):
     sess.run(train, feed_dict={X: x_data, Y: y_data})
     if step % 20 == 0:
-        print(step, sess.run(cost), feed_dict={X: x_data, Y: y_data}), sess.run(W)
+        print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W))
 
 
+print("===============================================")
+
+print(sess.run(hypothesis, feed_dict={X:[[1], [2], [2]]}) > 0.5)
+print(sess.run(hypothesis, feed_dict={X:[[1], [5], [5]]}) > 0.5)
+
+print(sess.run(hypothesis, feed_dict={X:[[1, 1], [4, 3], [3, 5]]}) > 0.5)
